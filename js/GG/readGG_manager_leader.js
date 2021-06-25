b7fc668fb04d6792e9067d0920d8c8a179e7c9ba
24together <to__get___her@naver.com>
2021-06-25 17:05:41 +0900

    create the chart JS

--- js/GG/readGG_manager_leader.js
+++ js/GG/readGG_manager_leader.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 
+
     function limitDate(){
         const firstDate = document.getElementById("first_date").value;
         var secondDate = document.getElementById("second_date");
@@ -31,5 +32,41 @@ window.onload = function() {
     }
 
     document.getElementById('second_date').addEventListener('click',limitDate);
-
+    
+    //chart js
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
